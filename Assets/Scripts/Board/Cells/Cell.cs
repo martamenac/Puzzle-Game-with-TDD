e52e037b2c11aff6c5b837ab648d3eb4d9ad8975
Marta Menac <martamenacayats@users.noreply.github.com>
2020-12-22 17:46:06 +0100

    Test WhenCreatingACellWithAColor_ThenTheCellHasTheCorrectColor()

---
+++ Assets/Scripts/Board/Cells/Cell.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell
+{
+    //public string color;
+    private Color _color;
+
+    public Color CellColor => _color;
+
+    public Cell()
+    {
+
+    }
+
+    public Cell(Color color)
+    {
+        _color = color;
+    }
+}
--- Assets/Scripts/Test/CellTests.cs
+++ Assets/Scripts/Test/CellTests.cs
@@ -8,6 +8,14 @@ namespace Tests
 {
     public class CellTests
     {
-        
+        [Test]
+        public void WhenCreatingACellWithAColor_ThenTheCellHasTheCorrectColor()
+        {
+            var expectedColor = Color.cyan;
+
+            var cell = new Cell(expectedColor);
+
+            Assert.AreEqual(expectedColor, cell.CellColor, $"Color of the created cell ({cell.CellColor}) is not correct ({expectedColor})");
+        }
     }
 }
